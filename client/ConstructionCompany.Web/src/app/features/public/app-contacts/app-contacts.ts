9401cdaa8410a0e82d7fc1d02bf0064df1f911f0
bluesbreak <ivanivanovat@gmail.com>
2025-07-12 11:45:31 +0300

    ContactFrom and SendMessage Implemented. PublicPage Finished

--- client/ConstructionCompany.Web/src/app/features/public/app-contacts/app-contacts.ts
+++ client/ConstructionCompany.Web/src/app/features/public/app-contacts/app-contacts.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contacts',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app-contacts.html',
   styleUrl: './app-contacts.scss',
 })
-export class AppContacts {}
+export class AppContacts {
+  sendEmail(e: Event) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_v8ivcfh',
+        'template_egpfg1c',
+        e.target as HTMLFormElement,
+        '6WRKoRcnSieEohhU3'
+      )
+      .then(
+        (response: EmailJSResponseStatus) => {
+          console.log('SUCCESS!', response.status, response.text);
+          alert('Message sent successfully!');
+        },
+        (error) => {
+          console.error('FAILED...', error);
+          alert('Oops — something went wrong. Please try again later.');
+        }
+      );
+  }
+}
